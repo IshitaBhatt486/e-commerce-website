e2c6d9bb42c9c4c0bcb9d2ffa497cf16295f3053
Ishita Bhatt <ishitabhatt049@gmail.com>
2026-08-08 16:42:07 +0530

    test

--- backend/authRoutes.js
+++ backend/authRoutes.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const { registerUser, loginUser, getUserDetails } = require('../controllers/authController');
+const { protect } = require('../middleware/authMiddleware');
+const {admin} = require('../middleware/adminMiddleware');
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.get("/user", getUserDetails);
+router.get("/users", protect, admin, getUserDetails);
+
+module.exports = router;
